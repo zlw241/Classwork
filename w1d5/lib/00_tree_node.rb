3a7df9a9ff4bc5dfbb5eb81f30126c3edde20fa3
= <=>
2017-03-03 13:28:20 -0500

    Revise insert, delete methods in PolyTreeNode

--- w1d5/lib/00_tree_node.rb
+++ w1d5/lib/00_tree_node.rb
@@ -1,42 +1,57 @@
 class PolyTreeNode
-  attr_accessor :children, :value, :par
+  attr_accessor :children, :value, :parent
 
   def initialize(value = nil)
     @value = value
     @children = []
-    @par = nil
+    @parent = nil
   end
 
   def parent=(node)
-    if self.par.nil?
-      @par = node
-      node.add_child(self)
-    else
-      node.add_child(self)
-      self.par.children.delete(self)
-      self.par = node
+    if node.nil?
+      @parent = nil
+      return
+    end
+    unless node.children.include? self
+      if self.parent.nil?
+        @parent = node
+        node.children << self
+      else
+        node.children << self
+        self.parent.children.delete(self)
+        @parent = node
+      end
     end
   end
 
-  def add_child(node)
-    self.children << node unless self.children.include?(node)
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    raise "Error" if child_node.parent.nil?
+    self.children.delete(child_node)
+    child_node.parent = nil
+  end
+
+  def dfs(target)
+    if self.value == target
+      return self
+    else
+      children.each { |child| return child.dfs(target) unless child.dfs(target).nil?}
+    end
   end
 end
 
 
-n1 = PolyTreeNode.new("root1")
-n2 = PolyTreeNode.new("root2")
-n3 = PolyTreeNode.new("root3")
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
 
 # connect n3 to n1
-n3.parent = n1
+# n3.parentent = n1
 # connect n3 to n2
-n3.parent = n2
-
-
-# this should work
-raise "Bad parent=!" unless n3.par == n2
-raise "Bad parent=!" unless n2.children == [n3]
-
-# this probably doesn't
-raise "Bad parent=!" unless n1.children == []
+# n3.add_child(n2)
+# p n1
+# p n2
+# p n3
